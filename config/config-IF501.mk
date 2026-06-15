2ea4fa51bf5932e2f99faa28952595dc634b745b
LukaMrt <maret.luka@gmail.com>
2026-06-15 09:04:15 +0200

    Revert "Merge pull request #6 from LukaMrt/feature/control"
    This reverts commit 245273dd1e855b508ebc147a50ceef5a22a68987, reversing
    changes made to 953ef543b0e249559d2d756b62642a8bac34fd80.

--- config/config-IF501.mk
+++ config/config-IF501.mk
@@ -1,3 +1,4 @@
-ANTLRJAR=/usr/share/java/antlr-complete.jar
-ANTLRINC=/usr/include/antlr4-runtime/
-ANTLRLIB=/usr/lib/libantlr4-runtime.a
+# these values work with the "install-antlr.sh" script provided for the PLD
+ANTLRJAR=../antlr/jar/antlr-4.9.2-complete.jar
+ANTLRINC=../antlr/include
+ANTLRLIB=../antlr/lib/libantlr4-runtime.a
