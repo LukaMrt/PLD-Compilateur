633b43fb7cb32d7983855bfb885bd1855d3f7359
LukaMrt <maret.luka@gmail.com>
2026-06-22 10:40:18 +0200

    Remove tests

--- tests/cases/106_error_decl_in_if.c
+++
@@ -1,6 +0,0 @@
-int main() {
-    int x = 5;
-    if (x > 3)
-        int y = 10; // Declaration in if without block
-    return x;
-}
--- tests/cases/115_error_pointer_type_mismatch.c
+++
@@ -1,8 +0,0 @@
-int foo(int *x) {
-    return *x;
-}
-
-int main() {
-    int a = 5;
-    return foo(a); // Passing non-pointer where pointer expected
-}
